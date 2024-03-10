04257b5250f240d81d82b662b700cae45a00eac9
Sandor Toth <toth.sandor.01@gmail.com>
2024-03-10 14:59:00 +0100

    Retrieving posts by similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # This class defines an alternative list view for blog posts using Django's generic ListView.
@@ -69,8 +70,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Instantiate the form for adding comments.
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
+    
     # Render the post detail template with the post, comments, and form included in the context.
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'form': form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
